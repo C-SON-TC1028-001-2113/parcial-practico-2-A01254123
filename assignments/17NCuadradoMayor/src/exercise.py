b9d549d556a119c793a39e822266e22ff0e00646
A01254123 <A01254123@itesm.mx>
2021-09-10 17:32:05 +0000

    ExamenPrueba2_CesarMtz

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -1,9 +1,9 @@
-
-
 def main():
-    numero = int(input("Escribe un numero : "))
     #escribe tu código abajo de esta línea
-
-
+    numero=int(input("Escribe un numero : "))
+    n=1
+    while n**2<=numero:
+        n=n+1
+    print(n) 
 if __name__=='__main__':
     main()
